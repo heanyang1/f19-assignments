1aebe5a1fd76317d8eacb65e9ee38760915d1e72
heanyang1 <e1124486@u.nus.edu>
2025-04-19 09:32:03 +0800

    finish final (lean)

--- final/lean/program/Final/evals.lean
+++ final/lean/program/Final/evals.lean
@@ -25,21 +25,43 @@ theorem evals_refl (e : Expr) : e ↦* e := evals.Val e
 
 theorem evals_trans {e1 e2 e3 : Expr} (h1 : e1 ↦* e2) (h2 : e2 ↦* e3)
   : e1 ↦* e3 := by
+    -- Currying: we can prove `(e2 ↦* e3) → (e1 ↦* e3)` given `h1: e1 ↦* e2`
+    -- Doing structural induction over `h1`
     induction h1
+    -- Base case: `e1 = e2`, then `h2` is what we want to prove
     case Val => assumption
+    -- Inductive case: `e1 ↦* e2` because `(e1 ↦ e_step)` and `e_step ↦* e2`
+    -- Inductive hypothesis (`ih`) is `(e2 ↦* e3) → (e_step ↦* e3)`
     case Step ih =>
-      apply evals.Step
-      assumption
-      exact ih h2
+      apply evals.Step  -- there must be steps from `e1` to `e3`
+      -- `evals.Step` creates goals `e1 ↦ e_step` and `e_step ↦* e3`
+      assumption  -- `e1 ↦ e_step`
+      exact ih h2 -- `e_step ↦* e3`
 
 -- @[trans] in Lean 3 is replaced by Trans typeclass
 instance: Trans (α := Expr) (· ↦* ·) (· ↦* ·) (· ↦* ·) where
   trans := evals_trans
 
 theorem transitive_left (b : Binop) (el el' er : Expr) :
-  (el ↦* el') → (Expr.Binop b el er ↦* Expr.Binop b el' er) :=
-  sorry -- Remove this line and add your proof
+  (el ↦* el') → (Expr.Binop b el er ↦* Expr.Binop b el' er) := by
+    intro h
+    induction h
+    case Val => rfl
+    case Step ih =>
+      apply evals.Step
+      apply steps.DLeft -- uses `el ↦* e_step` to create `Expr.Binop b el er ↦ Expr.Binop b e_step er`
+      assumption        -- `el ↦* el'`
+      exact ih
 
 theorem transitive_right (b : Binop) (el er er' : Expr) :
-  val el → (er ↦* er') → (Expr.Binop b el er ↦* Expr.Binop b el er') :=
-  sorry -- Remove this line and add your proof
+  val el → (er ↦* er') → (Expr.Binop b el er ↦* Expr.Binop b el er') := by
+    intro hval -- `val el`
+    intro h
+    induction h
+    case Val => rfl
+    case Step ih =>
+      apply evals.Step
+      apply steps.DRight -- similar to DLeft, but requires `val el`
+      assumption
+      exact hval
+      exact ih
--- final/lean/program/Final/totality.lean
+++ final/lean/program/Final/totality.lean
@@ -2,5 +2,42 @@ import Final.arith
 import Final.inversion
 import Final.evals
 
-theorem totality : ∀ e : Expr, ∃ e' : Expr, val e' ∧ e ↦* e' :=
-  sorry -- Remove this line and add your proof
+theorem totality : ∀ e : Expr, ∃ e' : Expr, val e' ∧ e ↦* e' := by
+  intro e
+  induction e
+  case Num n =>
+    exists Expr.Num n
+    apply And.intro (val.DNat n) (evals_refl (Expr.Num n))
+  case Binop op el er ihl ihr =>
+    -- left IH
+    apply Exists.elim ihl
+    intro el_val hl
+    have hll: ∃ n : Nat, el_val = Expr.Num n := val_inversion el_val hl.left
+    apply Exists.elim hll
+    intro nl hnl
+    -- right IH
+    apply Exists.elim ihr
+    intro er_val hr
+    have hrl: ∃ n : Nat, er_val = Expr.Num n := val_inversion er_val hr.left
+    apply Exists.elim hrl
+    intro nr hnr
+    -- the `e'`
+    exists Expr.Num (apply_binop op nl nr)
+    -- prove `val e' ∧ e ↦* e'`
+    constructor
+    -- left
+    apply val.DNat (apply_binop op nl nr)
+    -- right
+    have hstep: Expr.Binop op el_val er_val ↦ Expr.Num (apply_binop op nl nr) := by
+      simp [hnl, hnr]
+      apply steps.DOp op nl nr
+    calc
+      Expr.Binop op el er ↦* Expr.Binop op el_val er :=
+        transitive_left op el el_val er hl.right
+      _ ↦* Expr.Binop op el_val er_val :=
+        transitive_right op el_val er er_val hl.left hr.right
+      _ ↦* Expr.Num (apply_binop op nl nr) :=
+        evals.Step (Expr.Binop op el_val er_val)
+                   (Expr.Num (apply_binop op nl nr))
+                   (Expr.Num (apply_binop op nl nr))
+                   hstep (evals.Val (Expr.Num (apply_binop op nl nr)))
